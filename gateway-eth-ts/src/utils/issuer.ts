32913189e33638833015f2c9052409dd072dd3fb
Robert Leonard <rleonard@Roberts-MacBook-Pro.local>
2024-03-25 15:50:46 -0400

    Add utility method to resolve issuer config from a service endpoint

---
+++ gateway-eth-ts/src/utils/issuer.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export interface IssuerConfig {
+    displayName: string,
+    gatewayIssuerEndpoint: string
+}
+
+export const ISSUER_CONFIG_PATH = '/issuer-config.json';
+
+export const resolveIssuerConfigFromServiceEndpoint = async (serviceEnpointUrl: string) => {
+    // Call endpoint
+
+    const res = await axios.get(`${serviceEnpointUrl}${ISSUER_CONFIG_PATH}`);
+
+    if(res.status !== 200) { 
+        throw Error(await res.data); 
+    }
+    
+    const responseBody = res.data;
+
+    let issuerConfig: IssuerConfig;
+
+    // Verify name exist
+    if(!responseBody.displayName) { 
+        throw Error("Issuer configuration does not have a displayName"); 
+    }
+
+    // Verify url exist
+    if(!responseBody.gatewayIssuerEndpoint) { 
+        throw Error("Issuer configuration does not have a valid gatewayIssuerEndpoint"); 
+    }
+
+    issuerConfig =  { displayName: responseBody.displayName, gatewayIssuerEndpoint: responseBody.gatewayIssuerEndpoint };
+
+    return issuerConfig;
+}
